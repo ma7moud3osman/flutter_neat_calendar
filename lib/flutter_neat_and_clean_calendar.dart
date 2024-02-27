9a1ca4c100e52d66e2a628272112f194efbd09d2
Mahmoud Osman <m.3osman99@gmail.com>
2024-02-27 12:50:38 +0200

    fix: height overflow

--- lib/flutter_neat_and_clean_calendar.dart
+++ lib/flutter_neat_and_clean_calendar.dart
@@ -455,7 +455,7 @@ class _CalendarState extends State<Calendar> {
         child: Column(
           children: <Widget>[
             GridView.count(
-              childAspectRatio: 1.4,
+              childAspectRatio: 1.3,
               primary: false,
               shrinkWrap: true,
               crossAxisCount: 7,
